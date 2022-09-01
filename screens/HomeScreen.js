c7e5f6c1ade3d54ff088f6e6817ff09707e945d1
floridajoca <florida.joca@gmail.com>
2022-08-31 17:11:12 -0700

    Feature: Home Screen header

---
+++ screens/HomeScreen.js
@@ -0,0 +1,49 @@
+import React, {useLayoutEffect} from "react";
+import {Image, SafeAreaView, Text, TextInput, View} from "react-native";
+import {useNavigation } from "@react-navigation/native"
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+
+const HomeScreen = () => {
+
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, []);
+    return (
+            <SafeAreaView className='bg-white pt-5'>
+                <View className="flex-row pb-3 items-center  space-x-2 px-4">
+                    <Image
+                        source={{uri: "https://links.papareact.com/wru"}}
+                        className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+                    />
+                    <View className="flex-1">
+                        <Text className="font-bold text-gray-400 text-xs">
+                            Deliver Now!
+                        </Text>
+                        <Text className="font-bold text-xl">
+                            Current Location
+                            <FontAwesome name="chevron-down" size={20} color="#00CCBB"  />
+                        </Text>
+                    </View>
+                    <FontAwesome name="user" size={35} color="#00CCBB" />
+                </View>
+                {/*    Search bar*/}
+                <View className='flex-row items-center space-x-2 pb-2 px-4' >
+                    <View className='flex-row space-x-2 flex-1 bg-gray-200 p-3 items-center'>
+                        <FontAwesome name="search" size={20} />
+                        <TextInput
+                            placeholder={"Restaurants and cuisines"}
+                            keyboardType='default'
+                        />
+                    </View>
+                    <FontAwesome name="adjust" size={24} color="#00CCBB" />
+                </View>
+            </SafeAreaView>
+    )
+}
+
+export default HomeScreen;
